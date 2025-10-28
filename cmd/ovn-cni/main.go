c46791e5b4232e7c2aaf04e553849995b185ab3a
Vahid Alimohamadi <vahid.lid@live.com>
2025-10-28 11:57:05 +0330

    Prune.

--- cmd/ovn-cni/main.go
+++ cmd/ovn-cni/main.go
@@ -1,22 +1,14 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"os"
-	"time"
 
 	"github.com/containernetworking/cni/pkg/skel"
 	"github.com/containernetworking/cni/pkg/types"
 	types100 "github.com/containernetworking/cni/pkg/types/100"
 	"github.com/containernetworking/cni/pkg/version"
-	cniTypes "github.com/cybercoder/ik8s-ovn-cni/pkg/cni/types"
-	"github.com/cybercoder/ik8s-ovn-cni/pkg/k8s"
 	"github.com/cybercoder/ik8s-ovn-cni/pkg/net_utils"
-	"github.com/cybercoder/ik8s-ovn-cni/pkg/ovnnb"
-	"github.com/cybercoder/ik8s-ovn-cni/pkg/ovs"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func cmdAdd(args *skel.CmdArgs) error {
@@ -28,74 +20,27 @@ func cmdAdd(args *skel.CmdArgs) error {
 	defer f.Close()
 
 	log.SetOutput(f)
-	oclient, err := ovs.CreateOVSclient()
-	if err != nil {
-		return err
-	}
-	ovnClient, err := ovnnb.CreateOvnNbClient("tcp:192.168.12.177:6641")
-	if err != nil {
-		return err
-	}
 
-	k8sArgs := cniTypes.CniKubeArgs{}
-	if err := types.LoadArgs(args.Args, &k8sArgs); err != nil {
-		log.Printf("error loading args: %v", err)
-		return err
-	}
-	// 1. find kubevirt vm name using kube api
-	k8sClient, err := k8s.CreateClient()
-	if err != nil {
-		log.Printf("Error creating Kubernetes Client: %v", err)
-		return err
-	}
-	pod, err := k8sClient.CoreV1().Pods(string(k8sArgs.K8S_POD_NAMESPACE)).Get(context.Background(), string(k8sArgs.K8S_POD_NAME), metav1.GetOptions{})
-	if err != nil {
-		log.Printf("Error getting pod: %v", err)
-		return err
-	}
-	labels := pod.GetLabels()
-	log.Printf("the vm name is %s", labels["vm.kubevirt.io/name"])
-	vmName := labels["vm.kubevirt.io/name"]
-	hostIf := fmt.Sprintf("tap-%s", vmName)
-	if len(hostIf) > 15 {
-		hostIf = hostIf[:15]
-	}
-
-	err = oclient.AddManagedTapPort("br-int", hostIf)
-	if err != nil {
-		log.Printf("Error on Add managed tap port to br-int: %v", err)
-	}
-	realmac, err := oclient.WaitForPortMAC(hostIf, 30*time.Second)
-	if err != nil {
-		log.Printf("Error on getting mac address for %s: %v", hostIf, err)
-	}
-
-	// 4. Add port to ovn logical switch
-	log.Printf("mac address %s", realmac)
-	// err = ovnClient.CreateLogicalPort("public", hostIf, *containerMac)
-	err = ovnClient.CreateLogicalPort("public", hostIf, realmac)
-	if err != nil {
-		log.Printf("Error creating logical port on logical switch public: %v", err)
-		// return err
-	}
-	err = net_utils.BringInterfaceUp(hostIf)
+	log.Printf("ifName: %s", args.IfName)
+	veths, err := net_utils.GetVethList(args.Netns)
 	if err != nil {
 		log.Printf("%v", err)
 	}
-	// ✅ Build minimal CNI result
+
+	log.Printf("veth 0: %s", veths[0].Attrs().HardwareAddr.String())
+
 	result := &types100.Result{
 
 		CNIVersion: version.Current(),
 		Interfaces: []*types100.Interface{
 			{
 				Name:    args.IfName,
-				Mac:     realmac,
+				Mac:     veths[0].Attrs().HardwareAddr.String(),
 				Sandbox: args.Netns,
 			},
 		},
 	}
 
-	// ✅ Print JSON to stdout for CNI runtime
 	return types.PrintResult(result, version.Current())
 }
 
--- pkg/net_utils/net_utils.go
+++ pkg/net_utils/net_utils.go
@@ -1,123 +1,35 @@
 package net_utils
 
 import (
-	"crypto/md5"
 	"fmt"
-	"log"
-	"net"
-	"runtime"
 
+	"github.com/samber/lo"
 	"github.com/vishvananda/netlink"
 	"github.com/vishvananda/netns"
 )
 
-// CreateStableVeth creates a veth pair, keeps host side in current namespace
-// and moves peer side to the target netns (container).
-func CreateStableVeth(hostIf, ifName, netnsPath string) (*string, *string, error) {
-	runtime.LockOSThread()
-	defer runtime.UnlockOSThread()
-
-	peerIf := fmt.Sprintf("tmp-%s", hostIf)
-
+func GetVethList(netnsPath string) ([]netlink.Link, error) {
 	ns, err := netns.GetFromPath(netnsPath)
 	if err != nil {
-		return nil, nil, fmt.Errorf("failed to open target netns: %w", err)
+		return nil, fmt.Errorf("failed to enter target netns: %v", err)
 	}
 	defer ns.Close()
 
-	origNS, err := netns.Get()
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed to get current netns: %w", err)
-	}
-	defer origNS.Close()
-	defer func() {
-		if err := netns.Set(origNS); err != nil {
-			log.Printf("failed to restore netns: %v", err)
-		}
-	}()
-
-	veth := &netlink.Veth{
-		LinkAttrs: netlink.LinkAttrs{Name: hostIf, MTU: 1500},
-		PeerName:  peerIf,
-	}
-	if err := netlink.LinkAdd(veth); err != nil {
-		return nil, nil, fmt.Errorf("failed to create veth pair: %w", err)
-	}
-
-	hostLink, err := netlink.LinkByName(hostIf)
-	if err != nil {
-		return nil, nil, fmt.Errorf("host link lookup failed: %w", err)
-	}
-	if err := netlink.LinkSetUp(hostLink); err != nil {
-		return nil, nil, fmt.Errorf("failed to bring up host link: %w", err)
-	}
-	hostMAC := hostLink.Attrs().HardwareAddr.String()
-
-	peerLink, err := netlink.LinkByName(peerIf)
-	if err != nil {
-		return nil, nil, fmt.Errorf("peer link lookup failed: %w", err)
-	}
-	if err := netlink.LinkSetNsFd(peerLink, int(ns)); err != nil {
-		return nil, nil, fmt.Errorf("failed to move peer to target netns: %w", err)
-	}
-
 	if err := netns.Set(ns); err != nil {
-		return nil, nil, fmt.Errorf("failed to switch to target netns: %w", err)
+		return nil, fmt.Errorf("failed to enter target netns: %v", err)
 	}
 
-	peerLink, err = netlink.LinkByName(peerIf)
+	links, err := netlink.LinkList()
 	if err != nil {
-		return nil, nil, fmt.Errorf("failed to find peer in container ns: %w", err)
+		return nil, fmt.Errorf("failed to get link list in ns %s: %v", ns.String(), err)
 	}
 
-	if err := netlink.LinkSetName(peerLink, ifName); err != nil {
-		return nil, nil, fmt.Errorf("failed to rename peer: %w", err)
+	veths := lo.Filter(links, func(l netlink.Link, _ int) bool {
+		return l.Type() == "veth"
+	})
+
+	if len(veths) == 0 {
+		return nil, fmt.Errorf("no tap link in ns %s: %v", ns.String(), err)
 	}
-
-	if err := netlink.LinkSetUp(peerLink); err != nil {
-		return nil, nil, fmt.Errorf("failed to bring up peer: %w", err)
-	}
-	containerMAC := peerLink.Attrs().HardwareAddr.String()
-
-	log.Printf("✅ Created veth pair: host=%s (%s) ↔ container=%s (%s)", hostIf, hostMAC, ifName, containerMAC)
-
-	return &hostMAC, &containerMAC, nil
-}
-
-func GenerateMAC(unique string) (string, error) {
-	hash := md5.Sum([]byte(unique)) // 16 bytes
-	mac := net.HardwareAddr{
-		0x02,    // Locally administered, unicast
-		hash[0], // Use hash bytes for uniqueness
-		hash[1],
-		hash[2],
-		hash[3],
-		hash[4],
-	}
-	return mac.String(), nil
-}
-
-func BringInterfaceUp(nic string) error {
-	link, err := netlink.LinkByName(nic)
-	if err != nil {
-		return fmt.Errorf("cannot find host interface %s: %w", nic, err)
-	}
-	if err := netlink.LinkSetUp(link); err != nil {
-		return fmt.Errorf("failed to bring host interface up: %w", err)
-	}
-	return nil
-}
-
-func GetInterfaceMAC(ifName string) (string, error) {
-	link, err := netlink.LinkByName(ifName)
-	if err != nil {
-		return "", fmt.Errorf("failed to get link %s: %w", ifName, err)
-	}
-
-	mac := link.Attrs().HardwareAddr.String()
-	if mac == "" {
-		return "", fmt.Errorf("interface %s has no MAC address", ifName)
-	}
-
-	return mac, nil
+	return veths, nil
 }
